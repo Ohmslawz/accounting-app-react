45b592ec4cc996b4b48a6587adb47bd5d1e354bf
Tanapong Sa-nguansin <tanapong.s@outlier.co.th>
2024-06-25 13:43:42 +0700

    update version 5

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import {useState} from 'react' //นำ useState มาใช้
 
 function App() {
   const Design = {color:"darkblue", textAlign:"center"} //สร้างเป็นตัวแปรแล้วอิมพอตเข้าไปใน style ได้
-  const initData =[
-    {id:1,title:"ค่ารักษาพยาบาล",amount:2000},
-    {id:2,title:"ค่าน้ำมัน",amount:5000},
-    {id:3,title:"ค่าเช่าบ้าน",amount:25000},
-    {id:4,title:"ค่าเสื้อผ้า",amount:1000},
-  ]
-  const [items,setItems] = useState(initData) //สร้าง state ขึ้นมาเพื่อส่งค่าต่อให้ transaction component
+  const [items,setItems] = useState([]) //สร้าง state ขึ้นมาเพื่อส่งค่าต่อให้ transaction component
   const onAddNewItem = (newItem)=>{ //เป็นตัวแปรที่รับค่าข้อมูลที่ส่งมาจาก FormComponent
       setItems((prevItem)=>{
         return [newItem,...prevItem]
